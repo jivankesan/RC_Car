80141122eafdaa662430921e3333d840769025ec
JivanKesan <jkesan28@gmail.com>
2024-03-05 12:25:51 -0500

    got rid of pygmae

--- controls.py
+++ controls.py
@@ -1,34 +1,25 @@
-import pygame
+import inputs
 import os
-import sys
 
 class Controller:
     def __init__(self):
         """Initialize the joystick components"""
-        pygame.init()
-        pygame.joystick.init()
-        if pygame.joystick.get_count() == 0:
-            # No joysticks connected
-            print("No joystick detected")
-            pygame.quit()
-            sys.exit()
-        self.controller = pygame.joystick.Joystick(0)
-        self.controller.init()
-        self.axis_data = {i: 0.0 for i in range(self.controller.get_numaxes())}
+        self.devices = inputs.devices.gamepads
+        if not self.devices:
+            # No gamepad detected
+            print("No gamepad detected")
+            exit()
         
-        print('Controller initialized')
-
+        print("Gamepad detected")
+        
     def listen(self):
         """Listen for events to happen"""
-        for event in pygame.event.get():
-            if event.type == pygame.JOYAXISMOTION:
-                self.axis_data[event.axis] = round(event.value, 2)
-            elif event.type == pygame.QUIT:
-                # Handle the window being closed
-                pygame.quit()
-                sys.exit()
-
-        return self.axis_data
+        axis_data = {}
+        events = inputs.get_gamepad()
+        for event in events:
+            if event.ev_type == 'Absolute':
+                axis_data[event.code] = event.state
+        return axis_data
 
 if __name__ == "__main__":
     ps5_controller = Controller()
@@ -40,4 +31,3 @@ if __name__ == "__main__":
             print(axis_data)
     except KeyboardInterrupt:
         print("\nExiting program.")
-        pygame.quit()
