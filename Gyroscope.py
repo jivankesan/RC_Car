8a6f79a8a3936e24e74f28a500a909a65f4f4e2e
JivanKesan <jkesan28@gmail.com>
2024-03-06 16:48:46 -0500

    changes

--- Gyroscope.py
+++ Gyroscope.py
@@ -50,29 +50,24 @@ class GYRO:
             value -= 65536
         return value
     
-    def get_accel_data(self):
-        """Get acceleration data"""
+    def get_accel_gyro_data(self):
         acc_x = self.read_raw_data(self.ACCEL_XOUT_H)
         acc_y = self.read_raw_data(self.ACCEL_YOUT_H)
         acc_z = self.read_raw_data(self.ACCEL_ZOUT_H)
+        
+        gyro_x = self.read_raw_data(self.GYRO_XOUT_H)
+        gyro_y = self.read_raw_data(self.GYRO_YOUT_H)
+        gyro_z = self.read_raw_data(self.GYRO_ZOUT_H)
         
         Ax = acc_x / 16384.0
         Ay = acc_y / 16384.0
         Az = acc_z / 16384.0
         
-        return Ax, Ay, Az
-    
-    def get_gyro_data(self):
-        """Get gyroscope data"""
-        gyro_x = self.read_raw_data(self.GYRO_XOUT_H)
-        gyro_y = self.read_raw_data(self.GYRO_YOUT_H)
-        gyro_z = self.read_raw_data(self.GYRO_ZOUT_H)
-        
         Gx = gyro_x / 131.0
         Gy = gyro_y / 131.0
         Gz = gyro_z / 131.0
         
-        return Gx, Gy, Gz
+        return Ax, Ay, Az, Gx, Gy, Gz
     
     def compute_orientation(self):
         Ax, Ay, Az, Gx, Gy, Gz = self.get_accel_gyro_data()
