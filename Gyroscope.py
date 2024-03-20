0acf339b72dc96c1aeef28afe712fdded442afe6
JivanKesan <jkesan28@gmail.com>
2024-03-20 18:09:50 -0400

    changes

--- Gyroscope.py
+++ Gyroscope.py
@@ -1,30 +1,35 @@
 import time
+import serial
 import board
 import busio
 import adafruit_bno055
-import RPi.GPIO as GPIO
 
-GPIO.cleanup()
-
-GPIO.setmode(GPIO.BOARD)
-
-res = GPIO.getmode()
-
-print(res)
-
-"""
-    
-    
- 
-res = GPIO.getmode()
-print(res)
-
+# Initialize I2C
 i2c = busio.I2C(board.SCL, board.SDA)
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 
-while True:
-    euler = sensor.euler
-    print('X: {:.2f}, Y: {:.2f}, Z: {:.2f}'.format(euler[0], euler[1], euler[2]))
-    time.sleep(0.1)
+# Initialize serial port
+serial_port = '/dev/serial0'  # Update this to the correct serial port
+baud_rate = 9600  # Serial communication baud rate
+ser = serial.Serial(serial_port, baudrate=baud_rate, timeout=1)
 
-   """
+print("Publishing BNO055 heading to serial port")
+
+try:
+    while True:
+        # Get Euler angles (heading, roll, pitch)
+        euler = sensor.euler
+        if euler is not None:
+            heading = euler[0]  # Extract the heading value
+            print(f"Heading: {heading:.2f}")
+            
+            # Publish heading to serial port
+            ser.write(f"{heading:.2f}\n".encode())
+        
+        # Delay to avoid spamming
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Program terminated")
+finally:
+    # Close serial connection
+    ser.close()
