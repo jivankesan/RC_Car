239a2ab4b7fdb5dbfc72c48e845fff509bc6920d
JivanKesan <jkesan28@gmail.com>
2024-03-20 18:28:48 -0400

    changes

--- sensortest.py
+++ sensortest.py
@@ -8,9 +8,7 @@ import adafruit_bno055
 
 
 
-if __name__ == "__main__":
-    GPIO.setmode(GPIO.BOARD)
-    
+if __name__ == "__main__":  
     i2c = board.I2C()  
     sensor = adafruit_bno055.BNO055_I2C(i2c) 
     
