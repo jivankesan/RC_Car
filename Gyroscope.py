03f13a75568c2364f9e610fafb1b2afd71fcffdf
JivanKesan <jkesan28@gmail.com>
2024-03-20 17:55:27 -0400

    changes

--- Gyroscope.py
+++ Gyroscope.py
@@ -1,12 +1,16 @@
 import time
 import board
 import busio
-from adafruit_bno055 import BNO055
+import adafruit_bno055
 import RPi.GPIO as GPIO
 
 
 
-sensor = BNO055()
+res = GPIO.getmode()
+print(res)
+
+i2c = busio.I2C(board.SCL, board.SDA)
+sensor = adafruit_bno055.BNO055_I2C(i2c)
 
 while True:
     euler = sensor.euler
