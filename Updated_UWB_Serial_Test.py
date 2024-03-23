b02b9bc2b2ee34d3a4f0a7993b34f52cea69989c
Kish16 <kishanmuhundhan@gmail.com>
2024-03-22 23:24:37 -0400

    updated changes

--- Updated_UWB_Serial_Test.py
+++ Updated_UWB_Serial_Test.py
@@ -26,6 +26,7 @@ if __name__ == "__main__":
 
     x0 = np.array([0,0])
     points = [(0,0), (10,0), (0,10), (10,10)]
+    uwb_distances_dict = {}
     
     
     # Adjust port, baud rate, and timeout as needed
@@ -37,18 +38,23 @@ if __name__ == "__main__":
         try:
             while True:
                 distances = []
+                uwb_distances_dict = {}
                 for i in range(0,4):
                     # Read data from the serial port
                     data = ser.readline().decode().strip()
                     if data:  # Only print if data is not empty
-                        print("Received:", data)
-                        data.split(",")
-                    distances.append(int(data[2]))
+                        data = data.split(",")
+                        tag = int(data[0])
+                        distance = float(data[1])
+                        uwb_distances_dict[tag] = distance
+                        distances.append(int(data[2]))
                     # adjust order of points based on the uwb location accordingly
-                print(len(distances))
-                target_location = location_solver(points, distances, x0)
-                x0 = target_location
-                print("Target location:", target_location)
+                    target_location = location_solver(points, distances, x0)
+                    print("Target location:", target_location)
+                    print("Distances dictionary:", uwb_distances_dict)
+                    print("Distances count: ",len(distances))
+                    x0 = target_location
+                
                     
         except KeyboardInterrupt:
             print("\nExiting due to keyboard interrupt.")
