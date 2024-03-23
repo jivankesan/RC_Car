74321dac9c08d20d9453a2ebca23151673cbaffb
Kish16 <kishanmuhundhan@gmail.com>
2024-03-22 21:46:12 -0400

    updated uwb localization test changes

--- Updated_UWB_Serial_Test.py
+++ Updated_UWB_Serial_Test.py
@@ -44,13 +44,13 @@ if __name__ == "__main__":
                     data = ser.readline().decode().strip()
                     if data:  # Only print if data is not empty
                         data = data.split(",")
-                    tag = int(data[0])
-                    distance = float(data[1])
-                    uwb_distances_dict[tag] = distance
-                    distances.append(int(data[2]))
+                        tag = int(data[0])
+                        distance = float(data[1])
+                        uwb_distances_dict[tag] = distance
+                        distances.append(int(data[2]))
                     # adjust order of points based on the uwb location accordingly
                 print("Distances dictionary:", uwb_distances_dict)
-                print(len(distances))
+                print("Distances count: ",len(distances))
                 target_location = location_solver(points, distances, x0)
                 x0 = target_location
                 print("Target location:", target_location)
