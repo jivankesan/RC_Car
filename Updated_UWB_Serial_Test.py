9db89d034f261f5952e3481098c8f57a2071370c
Kish16 <kishanmuhundhan@gmail.com>
2024-03-22 23:07:56 -0400

    updated changes

--- Updated_UWB_Serial_Test.py
+++ Updated_UWB_Serial_Test.py
@@ -1,33 +1,3 @@
-'''
-    
-    # Adjust port, baud rate, and timeout as needed
-    try:
-        ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
-    except:
-        print("Failed to open serial port. Please check the port and try again.")
-    else:
-        try:
-            while True:
-                distances = []
-                uwb_distances_dict = {}
-                for i in range(0,4):
-                    # Read data from the serial port
-                    data = ser.readline().decode().strip()
-                    if data:  # Only print if data is not empty
-                        print("Received:", data)
-                        data.split(",")
-                        tag = int(data[0])
-                        distance = float(data[1])
-                        uwb_distances_dict[tag] = distance
-                    distances.append(int(data[2]))
-                    # adjust order of points based on the uwb location accordingly
-                print("Distances count: ",len(distances))
-                target_location = location_solver(points, distances, x0)
-                x0 = target_location
-                print("Target location:", target_location)
-                print("Distances dictionary:", uwb_distances_dict)
-'''
-
 import serial
 from scipy.optimize import minimize
 import math
