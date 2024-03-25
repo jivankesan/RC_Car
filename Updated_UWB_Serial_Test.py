297871b492a1044c1d391a3ad298318afca45400
yashpatel1314 <yashpatel1314@outlook.com>
2024-03-25 17:09:09 -0400

    Next Cpush

--- Updated_UWB_Serial_Test.py
+++ Updated_UWB_Serial_Test.py
@@ -13,7 +13,10 @@ class KalmanFilter:
         self.P = P0  # Initial covariance estimate
 
     def predict(self, u=None):
-        self.x = np.dot(self.A, self.x) + np.dot(self.B, u)
+        if self.B is not None:
+            self.x = np.dot(self.A, self.x) + np.dot(self.B, u)
+        else:
+            self.x = np.dot(self.A, self.x)
         self.P = np.dot(np.dot(self.A, self.P), self.A.T) + self.Q
 
     def update(self, z):
@@ -23,6 +26,7 @@ class KalmanFilter:
         self.x = self.x + np.dot(K, y)
         I = np.eye(self.x.shape[0])
         self.P = np.dot(np.dot(I - np.dot(K, self.H), self.P), (I - np.dot(K, self.H)).T) + np.dot(np.dot(K, self.R), K.T)
+
 
 def location_solver(points, distances, x0):
     def objective_func(X):
